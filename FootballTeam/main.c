6b9ab58e9444236920e60f34076d2f2e164572cd
krrsalmane <karrounsalmane@gmail.com>
2025-09-10 14:15:22 +0100

    display all the Players

--- FootballTeam/main.c
+++ FootballTeam/main.c
@@ -1,14 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define MAX_PLAYERS 100 // Define MAX_PLAYERS
 
-int main()
-{
-    int choice;
-    int choice2;
+struct Player {
+    int id;
+    int age;
+    int goals;
+    int shirtNumber;
+    char position[20];
+    char firstName[50];
+    char lastName[50];
+};
 
-    do
-    {
-        printf("\n=====  Football Team Management =====\n");
+int nextid = 1;
+struct Player team[MAX_PLAYERS];
+int playerCount = 0;
+
+struct Player addPlayer() { // Fixed syntax
+    struct Player p;
+    p.id = nextid++;
+
+    printf("Enter first name: ");
+    scanf("%s", p.firstName);
+    printf("Enter last name: ");
+    scanf("%s", p.lastName);
+    printf("Enter shirt number: ");
+    scanf("%d", &p.shirtNumber);
+    printf("Enter position: ");
+    scanf("%s", p.position);
+    printf("Enter age: ");
+    scanf("%d", &p.age);
+    printf("Enter how many goals: ");
+    scanf("%d", &p.goals);
+
+    // Basic validation
+    if (p.age < 0 || p.goals < 0 || p.shirtNumber < 0) {
+        printf("Invalid input! Age, goals, and shirt number must be non-negative.\n");
+        p.id = 0; // Mark as invalid (optional handling)
+    }
+
+    return p;
+}
+
+void displayPlayers() { // Removed unused parameter
+    if (playerCount == 0) {
+        printf("No players added yet!\n");
+        return;
+    }
+    for (int i = 0; i < playerCount; i++) {
+        printf("\nPlayer %d Information:\n", i + 1);
+        printf("ID: %d\n", team[i].id);
+        printf("Name: %s %s\n", team[i].firstName, team[i].lastName);
+        printf("Shirt Number: %d\n", team[i].shirtNumber);
+        printf("Position: %s\n", team[i].position);
+        printf("Age: %d\n", team[i].age);
+        printf("Goals: %d\n", team[i].goals);
+    }
+}
+
+int main() {
+    int choice, choice2;
+
+    do {
+        printf("\n===== Football Team Management =====\n");
         printf("1 - Add a Player\n");
         printf("2 - Display Players\n");
         printf("3 - Delete Player\n");
@@ -19,23 +73,28 @@ int main()
         printf("Select your choice: ");
         scanf("%d", &choice);
 
-        if (choice < 1 || choice > 7)
-        {
+        if (choice < 1 || choice > 7) {
             printf("Invalid choice!\n");
         }
 
-        switch (choice)
-        {
+        switch (choice) {
         case 1:
             printf("\n1 - Add a new Player\n");
             printf("2 - Add multiple Players\n");
             printf("Select your choice: ");
             scanf("%d", &choice2);
 
-            switch (choice2)
-            {
+            switch (choice2) {
             case 1:
-                printf("Add a new Player:\n");
+                if (playerCount < MAX_PLAYERS) {
+                    team[playerCount] = addPlayer();
+                    if (team[playerCount].id != 0) { // Check for valid player
+                        playerCount++;
+                        printf("Player added successfully!\n");
+                    }
+                } else {
+                    printf("Team is full!\n");
+                }
                 break;
             case 2:
                 printf("Add multiple Players:\n");
@@ -54,10 +113,9 @@ int main()
             printf("Select your choice: ");
             scanf("%d", &choice2);
 
-            switch (choice2)
-            {
+            switch (choice2) {
             case 1:
-                printf("List of all Players:\n");
+                displayPlayers(); // Fixed call
                 break;
             case 2:
                 printf("Players sorted by Name:\n");
@@ -85,22 +143,6 @@ int main()
             printf("3 - Modify Number of Goals\n");
             printf("Select your choice: ");
             scanf("%d", &choice2);
-
-            switch (choice2)
-            {
-            case 1:
-                printf("Modify Player's Position:\n");
-                break;
-            case 2:
-                printf("Modify Player's Age:\n");
-                break;
-            case 3:
-                printf("Modify Player's Number of Goals:\n");
-                break;
-            default:
-                printf("Invalid choice!\n");
-                break;
-            }
             break;
 
         case 5:
@@ -109,19 +151,6 @@ int main()
             printf("2 - By Name\n");
             printf("Select your choice: ");
             scanf("%d", &choice2);
-
-            switch (choice2)
-            {
-            case 1:
-                printf("Search Player by ID:\n");
-                break;
-            case 2:
-                printf("Search Player by Name:\n");
-                break;
-            default:
-                printf("Invalid choice!\n");
-                break;
-            }
             break;
 
         case 6:
@@ -133,28 +162,6 @@ int main()
             printf("5 - Youngest and Oldest Player\n");
             printf("Select your choice: ");
             scanf("%d", &choice2);
-
-            switch (choice2)
-            {
-            case 1:
-                printf("Total number of Players:\n");
-                break;
-            case 2:
-                printf("Average Age of Players:\n");
-                break;
-            case 3:
-                printf("Players with more than X Goals:\n");
-                break;
-            case 4:
-                printf("Best Scorer:\n");
-                break;
-            case 5:
-                printf("Youngest and Oldest Player:\n");
-                break;
-            default:
-                printf("Invalid choice!\n");
-                break;
-            }
             break;
 
         case 7:
@@ -164,7 +171,6 @@ int main()
         default:
             break;
         }
-
     } while (choice != 7);
 
     return 0;
